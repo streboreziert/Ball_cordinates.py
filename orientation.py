b8bc2b13ca0dd1fdd5b69a830cda2b052501bf09
Roberts Treize <93783050+streboreziert@users.noreply.github.com>
2024-06-25 22:08:30 +0300

    Update orientation.py

--- orientation.py
+++ orientation.py
@@ -61,3 +61,11 @@ cv2.arrowedLine(image, (cX1, cY1), (cX2, cY2), (255, 255, 0), 2)
 cv2.imshow('Direction of North Pole', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+sudo apt install build-essential cmake git pkg-config libgtk-3-dev \
+libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
+gfortran openexr libatlas-base-dev python3-dev python3-numpy \
+libtbb2 libtbb-dev libdc1394-22-dev
+
